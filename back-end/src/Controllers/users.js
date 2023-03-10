87afef31cf06f8143e141ddf44030dec37722c7d
Zogoury <david.zogoury125@gmail.com>
2023-03-10 10:16:28 +0100

    ajout des router login, signup, delete, update

--- back-end/src/Controllers/users.js
+++ back-end/src/Controllers/users.js
@@ -18,7 +18,7 @@ export async function logIn(req, res){
     if(!user) return res.status(400).json({message: "User not found"});
     if(user.password !== userLogin.password) return res.status(400).json({message: "Wrong password"});
     const token = jwt.signToken({id: user._id});
-    res.status(200).json({token: token, user: user});
+    res.status(200).json({token: token, user: {pseudo: user.pseudo, name: user.name}});
 };
 
 export async function secretUser(req, res) {
@@ -36,7 +36,7 @@ export async function secretUser(req, res) {
 export function logOut(req, res){
     res.status(200).json({message: "User logged out"});
 };
-
+//work in progress 
 export async function deleteUser(req, res){
     const reqData = req.params;
     await User.findByIdAndDelete(reqData.id);
@@ -45,16 +45,25 @@ export async function deleteUser(req, res){
 
 export async function updateUser(req, res){
     const userData = req.params;
-    const {pseudo, name, email, password} = req.body;
-    const newUser = await User.findByIdAndUpdate(userData.id, {pseudo, name, email, password});
-    res.status(200).json({user: newUser});
+    const {pseudo, name} = req.body;
+    const newUser = await User.findByIdAndUpdate(userData.id, {pseudo, name});
+    res.status(200).json({user: {name, pseudo}, message: "User updated"});
 };
 
-export function getUser(req, res){
-    const user = req.user;
-    res.status(200).json({user: user});
+export async function getUser(req, res){
+    const userData = req.params;
+    const authorization = req.headers.authorization.replace('Bearer ', '');
+    //bearer token
+    jwt.verifyToken(authorization, async (err, payload)=>{
+        if(err){ 
+            return res.status(401).json({message: 'Invalid token'});
+        }
+        res.status(200).json({user: payload});
+    }
+    );
+    
 };
-
+// work in progress
 export async function getUsers(req, res){
     const users = await User.findById();
     res.status(200).json({users: users});
